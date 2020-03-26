54912745c1179e6b6da76ebbf8c16d874e4c246a
David Petter Alves <cfdavidpetter@gmail.com>
2020-03-26 12:53:52 -0300

    Finished layout.

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn, FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+function NewIncident() {
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero"/>
+
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" /> Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Titulo do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default NewIncident;
