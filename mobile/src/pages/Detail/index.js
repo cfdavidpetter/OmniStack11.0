c07afbf2559b8bd382c777b82d91afab48779851
David Petter Alves <cfdavidpetter@gmail.com>
2020-03-27 13:10:56 -0300

    Created layout page of Detail.

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,63 @@
 import React from "react";
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+
+import logoImg from "../../assets/logo.png";
 
 export default function Detail() {
+  const nagevation = useNavigation();
+
+  function navegationBack() {
+    nagevation.goBack();
+  }
+
+  function sendEmail() {
+    alert('sendEmail');
+  }
+
+  function sendWhatsApp() {
+    alert('sendWhatsApp');
+  }
+
   return (
-    <View />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        
+        <TouchableOpacity onPress={navegationBack} >
+          <Feather name="arrow-left" size={28} color="#E02041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAD</Text>
+
+        <Text style={styles.incidentProperty}>CASO:</Text>
+        <Text style={styles.incidentValue}>Cachorro Atropelada</Text>
+
+        <Text style={styles.incidentProperty}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o Heroi desse caso.</Text>
+
+        <Text style={styles.heroDescription}>Entre em contato:</Text>
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp} >
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.action} onPress={sendEmail} >
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
